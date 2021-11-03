4cf41993460fa7689fd4a753c3650b3d62753400
15008181891 <383250208@qq.com>
2021-11-03 08:41:10 +0800

    add practice

--- lesson4/main.py
+++ lesson4/main.py
@@ -3,14 +3,35 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def practice1():
+    i = 1
+    while i <= 100:
+        print(i)
+        i += 1
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+def practice2():
+    n = int(input())
+    last = 0
+    current = 0
+    i = 0
+    while i <= n:
+        if i == 0:
+            current = 0
+        elif i == 1:
+            last = 0
+            current = 1
+        else:
+            currentHolder = last + current
+            last = current
+            current = currentHolder
+        print(current)
+        i += 1
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # practice1()
+    practice2()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
